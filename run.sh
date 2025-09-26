b9feb4f7ed0f97ea2a12df718a4d39bcf183d0dd
Kenyyhy <kennethhy.me@gmail.com>
2025-09-26 15:42:51 -0400

    Updates Java version to 17.
    Updates the Java source and target version to 17 in build.xml.
    Also updates the run script to use Java 17 and enable preview features.
    
    Includes cached player data.

--- run.sh
+++ run.sh
@@ -27,5 +27,5 @@ rm -f "$JAR"
 jar cfm "$JAR" META-INF/MANIFEST.MF -C "$BUILD_DIR" .
 rm -rf "$BUILD_DIR"
 
-exec java -cp "$JAR:$SCRIPT_DIR/lib/gson-2.6.2.jar" org.nemotech.rsc.Main "$@"
+exec java --enable-preview --source 17 -jar rsc.jar
 
